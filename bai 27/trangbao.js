89c193297ec3462603859277ca2e87d427a7cf17
PHAN VĂN TIẾN <hle53365@gmail.com>
2024-09-19 10:46:16 +0700

    Add files via upload

---
+++ bai 27/trangbao.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.read-more').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Chức năng đọc thêm sẽ được phát triển trong tương lai!');
+    });
+});
